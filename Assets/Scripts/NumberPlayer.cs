732a5204d8924f5933a8ac49a5c6f40b4dd601d4
Arquery <sgu_sigu@msn.com>
2020-01-28 13:26:03 -0700

    Diferent Characters
    Was modified PhotonManager script for can be change character to enter other users and created other prefabs for others players.

---
+++ Assets/Scripts/NumberPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberPlayer : MonoBehaviour
+{
+    public int playerNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNumber = Random.Range(1, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (playerNumber)
+        {
+            case 1:
+                gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("/marakame_idle_0");//"marakame_idle_0";
+                break;
+            case 2:
+                gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("/serpiente4_0");
+                break;
+            case 3:
+                gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("/jaguar_0");
+                break;
+            case 4:
+                gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("/jabali_corriendo_0");
+                break;
+        }
+    }
+}
--- Assets/Scripts/PhotonManager.cs
+++ Assets/Scripts/PhotonManager.cs
@@ -6,16 +6,33 @@ using Photon.Realtime;
 
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
+    private int random;
+    private string player;
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+        random = Random.Range(1, 4);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (random)
+        {
+            case 1:
+                player = "Player";
+                break;
+            case 2:
+                player = "Player 1";
+                break;
+            case 3:
+                player = "Player 2";
+                break;
+            case 4:
+                player = "Player 3";
+                break;
+        }
     }
 
     public override void OnConnectedToMaster()
@@ -30,7 +47,7 @@ public class PhotonManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.Instantiate("Player", new Vector2(Random.Range(-6f,6f),transform.position.y), Quaternion.identity);
+        PhotonNetwork.Instantiate(player, new Vector2(Random.Range(-6f,6f),transform.position.y), Quaternion.identity);
     }
 }
  
